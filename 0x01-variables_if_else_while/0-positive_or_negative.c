5d86d7e1825570563680008ffaf6e10d7fed323a
Daniel Berthran <berthlearn@gmail.com>
2022-12-09 18:48:56 -0800

    postive negative integer with c conditionals

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,22 +2,25 @@
 #include <time.h>
 #include <stdlib.h>
 
-int main(void) {
+/* main - determines if an integer is positive or negative
+ * n: the number in question
+ * Return: a printf statement 
+ */
+
+int main(void)
+{
 	/* local variable definition */
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX /2;
+	n = rand() - RAND_MAX / 2;
 	
 	/*Conditional statement*/
-	if (n < 0) {
+	if (n < 0)
 		printf("%d is negative\n", n);
-	}
-	else if (n > 0) {
+	else if (n > 0)
 		printf("%d is positive\n", n);
-	}
-	else {
+	else
 		printf("%d is zero\n", n);
-	}
 	return (0);
 }
