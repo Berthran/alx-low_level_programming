975b02e99e71796d823b7ee4e75f4f11bac4e44b
Daniel Berthran <berthlearn@gmail.com>
2022-12-11 09:16:20 -0800

    only digits with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - displays the number 0-9
+ * letters - a varable to contain the english alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	/* local variable definition */
+	int num;
+
+	/*Loop statement to display numbers*/
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
