c128c31196be7b8bb4af5aec751b8a5bd656a615
Daniel Berthran <berthlearn@gmail.com>
2022-12-11 08:32:30 -0800

    conditionals ans putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the english alphabet in lower case except the letters 'e' and 'q'
+ * letters - a varable to contain the english alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	/* local variable definition */
+	char letters;
+
+	/*Loop statement to display Lowercase*/
+	for (letters = 'a'; letters <= 'z'; letters++)
+		if (letters != 'e' && letters != 'q')
+			putchar(letters);
+	putchar('\n');
+	return (0);
+}
