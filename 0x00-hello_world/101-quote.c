49aa9cf7dcc0aea8c6060b322666f813ee25143c
Daniel Berthran <berthlearn@gmail.com>
2022-12-12 20:32:07 -0800

    display without functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - displays a text
  * Return: returns zero
  */
 
-int write(int filedes, const char *buf, unsigned int nbyte);
-int main(void )
+int main(void)
 {
-   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-   return 0;
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+   return (0);
 }
