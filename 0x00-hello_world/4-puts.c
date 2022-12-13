299e40db269238f39e7c84dc00647d35b1888889
Daniel Berthran <berthlearn@gmail.com>
2022-12-12 19:12:54 -0800

    basic puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - displays a text
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
