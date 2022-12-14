34c0dd4aca371ebc0d8b31d9932775ece64f6998
Daniel Berthran <berthlearn@gmail.com>
2022-12-13 19:50:28 -0800

    first header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * program prints the text _putchad
+ * Returns: if successful, 0
+ */
+
+int main(void)
+{
+	char text[] = "_putchar";
+	_putchar(text[0]);
+	_putchar(text[1]);
+	_putchar(text[2]);
+	_putchar(text[3]);
+	_putchar(text[4]);
+	_putchar(text[5]);
+	_putchar(text[6]);
+	_putchar(text[7]);
+	return (0);
+}
