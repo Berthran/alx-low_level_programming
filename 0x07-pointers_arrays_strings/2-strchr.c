9e9b7b1c5a90cfd2e76235b83c98de97a51ecf0f
Daniel Berthran <berthlearn@gmail.com>
2023-01-02 19:07:47 -0800

    move pointet to specific string character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: string variable
+ * @c: character to find in string
+ *
+ * Return: pointer to the first occurence of the character in string
+ */
+
+char *_strchr(char *s, char c)
+{	int i;
+	char *ps;
+	int str_len;
+
+	str_len = strlen(s);
+
+	for (i = 0; i < str_len; i++)
+	{
+		ps = &(*(s + i));
+		if (*(s + i) == c)
+			return (ps);
+	}
+	return (NULL);
+}
