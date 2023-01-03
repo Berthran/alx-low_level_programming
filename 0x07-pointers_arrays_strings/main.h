28c442a809b7a85922ad842686798de35a725855
Daniel Berthran <berthlearn@gmail.com>
2023-01-02 19:06:26 -0800

    initial commit

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <unistd.h>
+
+int _putchar(char c);
+
+char *_memset(char *s, char b, unsigned int n);
+
+char *_strchr(char *s, char c);
+
+#endif
