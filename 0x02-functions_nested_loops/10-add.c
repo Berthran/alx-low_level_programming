bd84a5c2e7073ca9144167578b6f740f8c02849e
Daniel Berthran <berthlearn@gmail.com>
2023-01-04 10:53:40 -0800

    add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -24,3 +24,5 @@ int _abs(int n);
 int print_last_digit(int n);
 
 void times_table(void);
+
+int add(int a, int b);
