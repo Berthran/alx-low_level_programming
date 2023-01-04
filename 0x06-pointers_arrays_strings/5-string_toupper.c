988a0127edc8be6c755a6cf7faf36d00d998149f
Daniel Berthran <berthlearn@gmail.com>
2023-01-04 07:18:52 -0800

    convert lower case characters to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * string_toupper - changes lower case to upper case
+ * @s: string literal
+ *
+ * Returns: all chaaractets in upper case
+ */
+
+char *string_toupper(char s[])
+{
+	int s_len = strlen(s);
+	int i = 0;
+
+	while (i <= s_len)
+	{
+		if (*(s + i) >= 97 && *(s + i) <= 122)
+			*(s + i) = *(s + i) - 32;
+		i++;
+	}
+	return (s);
+}
