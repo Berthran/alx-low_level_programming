deaa9c49b564ddfee7b8d0373949c2e51a7bf8ae
Daniel Berthran <berthlearn@gmail.com>
2023-01-09 05:44:55 -0800

    argument to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function to prints program
+ * @argc: count of function arguments
+ * @argv: vector of function arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	(void) argv;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
