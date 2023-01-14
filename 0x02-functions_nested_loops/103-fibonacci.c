148c07b06e6bf0070bef9590d355467bac277ec1
Daniel Berthran <berthlearn@gmail.com>
2023-01-14 12:24:08 -0800

    sum of even fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - sum the multiples of 3 and 5
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	long int i, num1, num2, next_num, sum;
+
+	num1 = 1;
+	num2 = 1;
+	sum = 0;
+
+	while (num2 <= 4000000)
+	{
+		if ((num2 % 2) == 0)
+			sum += i;
+		next_num = num1 + num2;
+		num1 = num2;
+		num2 = next_num;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
