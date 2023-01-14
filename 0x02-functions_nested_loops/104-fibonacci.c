78b1a3f680fcaa0ad8c6722c80fec26133bfcd50
Daniel Berthran <berthlearn@gmail.com>
2023-01-14 12:33:19 -0800

    98 fibonacci number

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - sum the multiples of 3 and 5
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	long int i, num1, num2, next_num;
+
+	num1 = 1;
+	num2 = 1;
+
+	for (i = 0; i <= 97; i++)
+	{
+		if (i == 97)
+			printf("%lu\n", num2);
+		else
+		{
+			printf("%lu, ", num2);
+			next_num = num1 + num2;
+			num1 = num2;
+			num2 = next_num;
+		}
+	}
+	return (0);
+}
