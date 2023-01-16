c62aa84a93c4eb072e1ddd94aa49f9727dbb85e1
Daniel Berthran <berthlearn@gmail.com>
2023-01-16 15:39:28 -0800

    print struct members

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - displays the values of the members of a structure
+ * @d: pointer to struct type
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	struct dog *pdog;
+
+	pdog = d;
+
+	if (d != NULL)
+	{
+		if (pdog->name)
+			printf("Name: %s\n", pdog->name);
+		else
+			printf("Name: (nil)\n");
+		if (pdog->age)
+			printf("Age: %.6f\n", pdog->age);
+		else
+			printf("Age: (nil)\n");
+		if (pdog->owner)
+			printf("Owner: %s\n", pdog->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
