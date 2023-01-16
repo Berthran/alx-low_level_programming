ffc8423c3a834f3137fd0a5b9689cd3c949cb110
Daniel Berthran <berthlearn@gmail.com>
2023-01-16 14:30:26 -0800

    my first C struct (header file)

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <stdlib.h>
+
+/**
+ * struct dog - collwction of data to describe a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
