8893feefba88a41c4f12106e935e1f5468b63515
Daniel Berthran <berthlearn@gmail.com>
2023-01-17 02:15:42 -0800

    free memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dog memory
+ * @d: pointer
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	struct dog *pdog;
+
+	pdog = malloc(sizeof(d));
+	free(pdog);
+}
