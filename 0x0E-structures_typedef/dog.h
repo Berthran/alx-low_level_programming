984e96d0726fe8230e3641c33c588fe420977fc8
Daniel Berthran <berthlearn@gmail.com>
2023-01-16 16:09:27 -0800

    first use of typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,8 +15,15 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - typedef for dog
+ */
+
 
 #endif
