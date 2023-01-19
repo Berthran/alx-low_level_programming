da293130baa74e7fb85c8b8c4e461d6161ce4c55
Daniel Berthran <berthlearn@gmail.com>
2023-01-19 07:49:12 -0800

    function pointer action on array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i = 0;
 
-	if (action != NULL)
+	if (array != NULL && action != NULL)
 	{
 		while (i < size)
 		{
