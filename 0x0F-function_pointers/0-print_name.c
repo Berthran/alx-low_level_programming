9ba2283e691b0f1a220ff1a2b1f1c09b0539c194
Daniel Berthran <berthlearn@gmail.com>
2023-01-19 07:21:10 -0800

    first function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints given name
+ * @name: name to print
+ * @f: function pointer to format to print name
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <unistd.h>
+
+int _putchar(char c);
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
