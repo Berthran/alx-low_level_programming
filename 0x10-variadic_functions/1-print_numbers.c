bd5eb5ecfcaee3b4209bd002f072ebfddeb58cff
Daniel Berthran <berthlearn@gmail.com>
2023-01-24 10:54:24 -0800

    handle errors

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,13 +14,23 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	if (separator != NULL && n != 0)
+	if (n != 0)
 	{
 		va_start(ap, n);
-
-		for (i = 0; i < n - 1; i++)
-			printf("%d%s", va_arg(ap, int), separator);
+		if (separator != NULL)
+		{
+			for (i = 0; i < n - 1; i++)
+				printf("%d%s", va_arg(ap, int), separator);
+		}
+		else
+		{
+			for (i = 0; i < n - 1; i++)
+				printf("%d", va_arg(ap, int));
+		}
 		printf("%d\n", va_arg(ap, int));
 		va_end(ap);
 	}
+	else
+		printf("\n");
+
 }
