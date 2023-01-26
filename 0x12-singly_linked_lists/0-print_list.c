5f485e48044657c9a2aaf475a1cf912571653554
Daniel Berthran <berthlearn@gmail.com>
2023-01-26 15:53:30 -0800

    printing number of linked list nodes (pushed from ALX intranet)

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,15 +6,17 @@
  * print_list - prints all the elements of a list of type list_t
  * @h: pointer to the struct list_t
  *
- * Returns: number of nodes of the linked list
+ * Return: number of nodes of the linked list
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t eleCounter = 0; 
-	// counts the number of elements in the linked list
-	
-	while (h->next)
+	size_t eleCounter = 0;
+	/** counts the number of elements in the linked list*/
+	if (h == NULL)
+		return (-1);
+
+	while (h)
 	{
 		if (h->str)
 		{
