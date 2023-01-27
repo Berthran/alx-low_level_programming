89165db41a48701e78cfed873221cf819ef51912
Daniel Berthran <berthran146@gmail.com>
2023-01-27 01:58:23 +0100

    let's add some nodes

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stlib.h>
+#include <string.h>
+
+/**
+ * add_node - adds a node to a node pointer
+ * @head: address or pointer to the current node head
+ * @str: string value to write into node str member
+ *
+ * Return: address to new element of node or NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t char_len;
+	list_t temp;
+
+	temp = malloc(sizeof(list_t));
+
+	if (temp && str)
+	{
+		char_len = strlen(str);
+		temp->str = strdup(str);
+		temp->len = char_len;
+		temp->next = *head;
+		*head = temp;
+	}
+	return (*head);
+}
+	
