cc54c7566484603ebb9ad86bf96dd6bf7fac486a
Daniel Berthran <berthlearn@gmail.com>
2023-01-29 13:57:24 -0800

    string concatenation (ALX intranet) - check 7

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
-	pStr = (char *)malloc((s1_len + s2_len) * sizeof(char));
+	pStr = (char *)malloc((1 + s1_len + s2_len) * sizeof(char));
 
 	/* Handle memory allocation error */
 	if (pStr == NULL)
