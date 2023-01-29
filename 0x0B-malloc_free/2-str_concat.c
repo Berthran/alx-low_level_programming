1d1eaf00ba3e721fd61647d846be4285b0b810e0
Daniel Berthran <berthlearn@gmail.com>
2023-01-29 13:54:37 -0800

    string concatenation (ALX intranet)

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,9 @@ char *str_concat(char *s1, char *s2)
 {
 	char *pStr;
 	size_t i, j, s1_len, s2_len;
-	
+
+	j = 0;
+
 	/* Handle NULL strings by converting to empty strings*/
 	if (s1 == NULL)
 		s1 = "";
@@ -23,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
-	pStr = (char*)malloc((s1_len + s2_len) * sizeof(char));
+	pStr = (char *)malloc((s1_len + s2_len) * sizeof(char));
 
 	/* Handle memory allocation error */
 	if (pStr == NULL)
@@ -32,12 +34,13 @@ char *str_concat(char *s1, char *s2)
 	/* Write s1 characters to memory*/
 	for (i = 0; i < s1_len; i++)
 		pStr[i] = s1[i];
-	
-	/* Continue with i to go further in memory space and j to 
-	 * begin with s2*/
+	/*
+	 * Continue with i to go further in memory space and j to
+	 * begin with s2
+	 */
 	while (i < (s1_len + s2_len))
 	{
-		PStr[i] = s2[j];
+		pStr[i] = s2[j];
 		++j;
 		++i;
 	}
