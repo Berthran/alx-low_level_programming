73662ef72ba50a2a9992fa6405c9d08a09bd26f6
Daniel Berthran <berthran146@gmail.com>
2023-01-31 03:02:16 +0100

    free a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_listint - frees up allocated memory of a list
+ * @head: pointer to beginning of list
+ *
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *p;
+
+	if (head != NULL)
+	{
+		while (head->next != NULL)
+		{
+			p = head;
+			head = head->next;
+			free(p);
+		}
+	}
+}
