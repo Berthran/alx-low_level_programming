4a82ffc7d4c0efd1b3f857dcb0ab25c66f699269
Daniel Berthran <berthran146@gmail.com>
2023-01-31 14:26:40 +0100

    sum linked list elements (if ints)

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_listint - calculates the sum of elements in a linked list
+ * @head: beginning of the list
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *int_get;
+
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	int_get = head;
+
+	while (int_get)
+	{
+		sum += int_get->n;
+		int_get = int_get->next;
+	}
+	return (sum);
+}
