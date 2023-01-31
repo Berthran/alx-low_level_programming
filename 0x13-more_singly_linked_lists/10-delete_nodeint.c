3619c2d8b8c3231ff64ac7b9b4c20a8902c4c358
Daniel Berthran <berthran146@gmail.com>
2023-02-01 00:52:02 +0100

    delete node at index of choice

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index - deletes the node at index 'index' of a linked list
+ * @head: beginning of the list
+ * @index: position of the node to delete
+ *
+ * Return: 1 if successful or -1 if unsuccessful
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node_crawler, *node_to_del, *nodes_after;
+
+	unsigned int i = 0;
+	nodes_after = NULL;
+	
+	if (head == NULL)
+		return (-1);
+	node_crawler = *head;
+
+	if (index == 0)
+	{
+		*head = node_crawler->next;
+		node_crawler->next = NULL;
+		free(node_crawler);
+		return (1);
+	}
+	while (i < (index - 1))
+	{
+		if (node_crawler->next == NULL)
+			return (-1);
+		node_crawler = node_crawler->next;
+		++i;
+	}
+	node_to_del = node_crawler->next;
+	
+	if (node_to_del->next)
+		nodes_after = node_to_del->next;
+	node_crawler->next = nodes_after;
+	free(node_to_del);
+	return (1);
+}
