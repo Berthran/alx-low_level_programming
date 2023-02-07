7d441e645368c3e3e943ffab9dc0653900f6cb75
Daniel Berthran <berthlearn@gmail.com>
2023-02-07 11:34:45 -0800

    read and write sequence re-ordering

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,18 +17,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd1, fd2, fd3;
 	char *buf;
 
-	buf = (char *)malloc(letters * sizeof(char));
 
-	if (buf == NULL || filename == NULL)
+	if (filename == NULL)
 		return (0);
 
 	fd1 = open(filename, O_RDONLY);
-	fd2 = read(fd1, buf, letters);
-	if (fd1 < 0 || fd2 < 0)
-	{
-		free(buf);
+	if (fd1 == -1)
 		return (0);
-	}
+
+	buf = (char *)malloc(letters * sizeof(char));
+	if (buf == NULL)
+		return (0);
+
+	fd2 = read(fd1, buf, letters);
+	if (fd2 == -1)
+		return (0);
 
 	fd3 = write(STDOUT_FILENO, buf, fd2);
 	if (fd3 < 0)
@@ -37,5 +40,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	free(buf);
-	return (fd2 & fd3);
+	return (fd2);
 }
