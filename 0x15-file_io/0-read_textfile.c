21e47315d9c36c200c3d5c6b40bc375bc4a0ed96
Daniel Berthran <berthlearn@gmail.com>
2023-02-07 11:55:40 -0800

    read and write sequence re-ordering debugged IV

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,6 +26,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd1 == -1)
 		return (0);
 
+	if (letters <= 0)
+		return (0);
+
 	buf = (char *)malloc(letters * sizeof(char));
 	if (buf == NULL)
 		return (0);
