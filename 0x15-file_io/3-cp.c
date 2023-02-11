08e02fe0ef35d6f81c5d6d063c83c6b97fa28e16
Daniel Berthran <berthlearn@gmail.com>
2023-02-11 15:34:32 +0100

    building a prototype of syscall

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,34 +15,98 @@
 
 int main(int argc, char **argv)
 {
-	if (argc != 2)
+	int fd1, fd2, fd3, fd4;
+	char *buf;
+
+	if (argc != 3)
 	{
-		char *errmsg = "Usage: cp file_from file_to\n";
-		size_t bytes = strlen(errmsg);
-		write(STDERROR_FILENO, errmsg, bytes);
-		exit (97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-
-	if (filename == NULL)
-		return (-1);
-
-	fd1 = open(filename, O_WRONLY | O_APPEND);
+	fd1 = open_file(argv[1], O_RDONLY, 0, 0);
 	if (fd1 == -1)
-		return (-1);
-
-	if (text_content == NULL)
+		exit (98);
+	buf = (char *)malloc(1024 * sizeof(char));
+	fd2 = readwrite_file(fd1, buf, 1024, argv[1], 0);
+	if (fd2 == -1)
 	{
-		if (fd1 != -1)
-		{
-			close(fd1);
-			return (1);
-		}
+		free(buf);
+		if (close_fd(fd1) == -1)
+			exit(100);
+		exit (98);
+	}
+	fd3 = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664, 1);
+	if (fd3 == -1)
+	{
+		free(buf);
+		if (close_fd(fd1) == -1 || close_fd(fd2) == -1)
+			exit(100);
+		exit (99);
+	}
+	fd4 = readwrite_file(fd3, buf, fd2, argv[2], 1);
+	if (fd4 == -1)
+	{
+		free(buf);
+		if ((close_fd(fd1) == -1 || close_fd(fd2) == -1) || close_fd(fd3) == -1)
+			exit(100);
+		exit(99);
+	}
+	free(buf);
+	if ((close_fd(fd1) == -1 || close_fd(fd2) == -1) || (close_fd(fd3) == -1 || close_fd(fd4) == -1))
+		exit(100);
+	return (0);
+}
+
+	
+int close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		return (-1);
 	}
+	return (0);
+}
 
-	while (*text_content)
-		write(fd1, text_content++, 1);
-	close(fd1);
-	return (1);
+int open_file(char *filename, int flags, int mode, int err)
+{
+	int fd1;
+
+	fd1 = open(filename, flags, mode);
+	if (fd1 == -1)
+	{
+		if (err == 0)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		else
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
+		return (-1);
+	}
+	return (fd1);
+}
+
+int readwrite_file(int fd, char *buffer, size_t bytes, char *filename, int call)
+{
+	int fd1;
+
+	if (call == 0)
+	{
+		fd1 = read(fd, buffer, bytes);
+		if (fd1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+			return (-1);
+		}
+		return (fd1);
+	}
+	else
+	{
+		fd1 = write(fd, buffer, bytes);
+		if (fd1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+			return (-1);
+		}
+		return (fd1);
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,11 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+int close_fd(int fd);
+
+int open_file(char *filename, int flags, int mode, int err);
+
+int readwrite_file(int fd, char *buffer, size_t bytes, char *filename, int call);
+
 
 #endif
