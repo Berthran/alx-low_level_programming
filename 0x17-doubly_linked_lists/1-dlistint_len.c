f01578cf07ee1926de782285652de0723efddd96
Daniel Berthran <berthlearn@gmail.com>
2023-03-16 14:02:32 +0100

    count nodes of dlist

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of nodes in a doubly linked list
+ * @h: the dlist header
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *dlist_ptr;
+	size_t nodes_count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	dlist_ptr = h;
+	while (dlist_ptr != NULL)
+	{
+		dlist_ptr = dlist_ptr->next;
+		nodes_count += 1;
+	}
+	return (nodes_count);
+}
