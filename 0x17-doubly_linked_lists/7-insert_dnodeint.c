1233048d1d71d87cdbdb59fbdf1eb285291bc1ef
Daniel Berthran <berthlearn@gmail.com>
2023-03-17 01:47:30 +0100

    dlist insertion

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position to a dlist
+ * @h: the beginning of the dlist
+ * @idx: position to insert new node at
+ * @n: integer value of new node
+ *
+ * Return: address of new dlist
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_dlistint_t, *head_crawler, *prev_node;
+	unsigned int no_of_nodes, node = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	no_of_nodes = count_dlistint_nodes(*h);
+
+	new_dlistint_t = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_dlistint_t == NULL)
+		return (NULL);
+
+	head_crawler = *h;
+	new_dlistint_t->n = n;
+
+	if (idx == 0)
+	{
+		new_dlistint_t->prev = *h;
+		new_dlistint_t->next = NULL;
+		*h = new_dlistint_t;
+	}
+
+	else if (idx > 0 && idx < no_of_nodes)
+	{
+		while (head_crawler != NULL)
+		{
+			prev_node = head_crawler;
+			head_crawler = head_crawler->next;
+			node += 1;
+			if (node == idx)
+			{
+				new_dlistint_t->prev = prev_node;
+				new_dlistint_t->next = head_crawler;
+				prev_node->next = new_dlistint_t;
+			}
+		}
+	}
+	else if (idx == no_of_nodes)
+	{
+		while (head_crawler != NULL)
+			head_crawler = head_crawler->next;
+		new_dlistint_t->prev = head_crawler;
+		head_crawler->next = new_dlistint_t;
+		new_dlistint_t->next = NULL;
+	}
+	return (*h);
+}
+
+/**
+ * count_nodes - count no of nodes in a dlist
+ * @head: the beginnig of the node
+ *
+ * Return: the number of nodes counted
+ */
+
+size_t count_dlistint_nodes(dlistint_t *head)
+{
+	dlistint_t *dlist_ptr;
+	size_t nodes_count = 0;
+
+	if (head == NULL)
+		return (0);
+
+	dlist_ptr = head;
+	
+	while (dlist_ptr != NULL)
+	{
+		dlist_ptr = dlist_ptr->next;
+		nodes_count += 1;
+	}
+	return (nodes_count);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -24,6 +24,8 @@ size_t print_dlistint(const dlistint_t *h);
 
 size_t dlistint_len(const dlistint_t *h);
 
+size_t count_dlistint_nodes(dlistint_t *head);
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
@@ -33,5 +35,7 @@ void free_dlistint(dlistint_t *head);
 int sum_dlistint(dlistint_t *head);
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 #endif
