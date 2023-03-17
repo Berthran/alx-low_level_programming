fcd012ead27b106d2d31bb5c50a19d4ba87728cf
Daniel Berthran <berthlearn@gmail.com>
2023-03-17 03:52:45 +0100

    dlist deletion - betty fix I

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -40,4 +40,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
+int del_dlist_beginning(dlistint_t **head, unsigned int index);
+
 #endif
