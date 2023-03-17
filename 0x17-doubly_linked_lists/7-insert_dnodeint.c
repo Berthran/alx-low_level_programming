19e4a5e1abebdac9853487804e9401ba466b489c
Daniel Berthran <berthlearn@gmail.com>
2023-03-17 03:03:50 +0100

    dlist insertion - code fix VII

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_dlistint_t->prev = NULL;
 		new_dlistint_t->next = *h;
 		*h = new_dlistint_t;
-		return (*h);
+		return (new_dlistint_t);
 	}
 
 	else if (idx > 0 && idx < no_of_nodes)
