8c809276f67d2f3519dde2bac85a9d2a21c32a58
Daniel Berthran <berthlearn@gmail.com>
2024-01-15 12:56:59 +0100

    program to concatenate two strings together at runtime using dynamic malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+
+char *null_checker(char *str);
+int count_chars(char *str);
+char *write_chars(char *read_str, char *write_str, int position, int str_len);
 
 /**
  * str_concat - concatenates two strings
@@ -12,38 +16,78 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *pStr;
-	size_t i, j, s1_len, s2_len;
-
-	j = 0;
+	int s1_len, s2_len;
+	char *str_concat;
 
 	/* Handle NULL strings by converting to empty strings*/
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	s1 = null_checker(s1);
+	s2 = null_checker(s2);
 
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
-	pStr = (char *)malloc((1 + s1_len + s2_len) * sizeof(char));
+	s1_len = count_chars(s1);
+	s2_len = count_chars(s2);
+
+	/* Allocate memory for s1 and s2 concatenated */
+	str_concat = (char *)malloc((1 + s1_len + s2_len) * sizeof(char));
 
 	/* Handle memory allocation error */
-	if (pStr == NULL)
+	if (str_concat == NULL)
 		return (NULL);
 
-	/* Write s1 characters to memory*/
-	for (i = 0; i < s1_len; i++)
-		pStr[i] = s1[i];
-	/*
-	 * Continue with i to go further in memory space and j to
-	 * begin with s2
-	 */
-	while (i < (s1_len + s2_len))
+	/* Write characters to allocated memory*/
+	str_concat = write_chars(s1, str_concat, 0, s1_len);
+	str_concat = write_chars(s2, str_concat, s1_len, s2_len);
+	return (str_concat);
+}
+
+/**
+ * null_checker - checks if strings are null
+ * @str: string to check
+ *
+ * Return: original string if not null or empty string if null
+ */
+
+char *null_checker(char *str)
+{
+	if (str == NULL)
+		return ("");
+	return (str);
+}
+
+/**
+ * count_chars - counts the number of characters in a string
+ * @str: the string of characters to count
+ *
+ * Return: the length of the string
+ */
+
+int count_chars(char *str)
+{
+	int counter = 0;
+
+	while (*str++)
+		counter++;
+	return (counter);
+}
+
+/**
+ * write_chars - writes the characters of one
+ * string to another at a given position
+ * @read_str: string to copy from
+ * @write_str: string to write to
+ * @position: position to begin writing to write_str
+ * @str_len: length of read_str
+ *
+ * Return: the write_str
+ */
+
+char *write_chars(char *read_str, char *write_str, int position, int str_len)
+{
+	int counter = 0;
+
+	while (counter < str_len)
 	{
-		pStr[i] = s2[j];
-		++j;
-		++i;
+		write_str[position + counter] = read_str[counter];
+		counter++;
 	}
-	pStr[i] = '\0';
-	return (pStr);
+	return (write_str);
 }
---
+++ 0x0B-malloc_free/2-str_concat_old.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *null_checker(char *str);
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string character
+ * @s2: second string character
+ *
+ * Return: pointer to memory address of concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *pStr;
+	size_t i, j, s1_len, s2_len;
+
+	j = 0;
+
+	/* Handle NULL strings by converting to empty strings*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	pStr = (char *)malloc((1 + s1_len + s2_len) * sizeof(char));
+
+	/* Handle memory allocation error */
+	if (pStr == NULL)
+		return (NULL);
+
+	/* Write s1 characters to memory*/
+	for (i = 0; i < s1_len; i++)
+		pStr[i] = s1[i];
+	/*
+	 * Continue with i to go further in memory space and j to
+	 * begin with s2
+	 */
+	while (i < (s1_len + s2_len))
+	{
+		pStr[i] = s2[j];
+		++j;
+		++i;
+	}
+	pStr[i] = '\0';
+	return (pStr);
+}
+
+
+/**
+ * check_null - checks if strings are null
+ * @str: string to check
+ *
+ * Return: original string if not null or empty string if null
+ */
+
+char *null_checker(char *str)
+{
+	if (*str == NULL || str == NULL)
+		return ("");
+	return (str);
+}
