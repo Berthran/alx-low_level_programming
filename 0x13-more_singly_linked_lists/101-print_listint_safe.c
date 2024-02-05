c90cd4afae0531a11b75ad191efa493ffb17bfa6
Daniel Berthran <berthlearn@gmail.com>
2024-02-05 12:33:23 +0100

    printing singly linked lists with memory address

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint_safe - prints the element and memory address of a linked list
+ * @head: the beginning of the linked list
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int n;
+	const void *ptr = head;
+	size_t n_nodes = 0;
+
+	if (head == NULL)
+		exit(98);
+
+	while (head != NULL && (void *)head <= ptr)
+	{
+		n = head->n;
+		printf("[%p] %d\n", (void *)head, n);
+		ptr = head;
+		head = head->next;
+		n_nodes += 1;
+	}
+	return (n_nodes);
+}
