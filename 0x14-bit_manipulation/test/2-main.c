e01f941487e472f96229b2036e39f9ee52d83aab
Daniel Berthran <berthlearn@gmail.com>
2024-03-15 01:50:27 +0100

    test file for task 2

---
+++ 0x14-bit_manipulation/test/2-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <limits.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = get_bit(2048, 11);
+    printf("%d\n", n);
+    n = get_bit(98, 0);
+    printf("%d\n", n);
+    n = get_bit(ULONG_MAX, 20000);
+    printf("%d\n", n);
+    return (0);
+}
