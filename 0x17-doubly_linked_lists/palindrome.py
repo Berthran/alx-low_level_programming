93cecff012bccb02e918971357c0752d285e5058
Daniel Berthran <berthlearn@gmail.com>
2024-03-21 18:31:38 +0100

    palindrominc

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -4,9 +4,10 @@ def max_palindrome(pals=[]):
     for i in range(100, 1000):
         for j in range(100, 1000):
             prod = i * j
+            prods = str(i) + " x " + str(j)
             prod_str = str(prod)
             if prod_str == prod_str[::-1]:
-                pals.append(prod)
+                pals.append(prods)
     return (pals[-1])
 
 pals = []
