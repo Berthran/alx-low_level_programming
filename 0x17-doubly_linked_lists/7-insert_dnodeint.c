c0b129c1c76a344e9edcb31fe63434d96cb8bf46
Daniel Berthran <berthlearn@gmail.com>
2024-03-22 13:25:04 +0100

    return address of new node not node head

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_dlistint_t, *head_crawler, *prev_node;
 	unsigned int no_of_nodes, node = 0;
+
 	new_dlistint_t = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 	if (h == NULL || new_dlistint_t == NULL)
@@ -44,9 +45,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				new_dlistint_t->prev = prev_node;
 				new_dlistint_t->next = head_crawler;
 				prev_node->next = new_dlistint_t;
+				return (new_dlistint_t);
 			}
 		}
-		return (*h);
+		/*return (new_dlistint);*/
 	}
 	else if (idx == no_of_nodes)
 	{
@@ -55,7 +57,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_dlistint_t->prev = head_crawler;
 		head_crawler->next = new_dlistint_t;
 		new_dlistint_t->next = NULL;
-		return (*h);
+		return (new_dlistint_t);
 	}
 	return (NULL);
 }
