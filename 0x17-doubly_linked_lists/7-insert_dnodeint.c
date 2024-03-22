3163f654b027ebee70fa0a98a168c0dca92be12c
Daniel Berthran <berthlearn@gmail.com>
2024-03-22 14:25:12 +0100

    create function to insert node between the end and the beginning of the DLL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,6 +25,40 @@ dlistint_t *insert_dnodeint_at_0(dlistint_t **h, dlistint_t **new_node)
 }
 
 /**
+ * insert_dnodeint_at_mid - inserts a node in between the end and beginning
+ * @h: the beginning of the list
+ * @new_node: the address of the new node
+ * @idx: the position to insert the new node at
+ *
+ * Return: the address of the new node
+ */
+
+dlistint_t *insert_dnodeint_at_mid(dlistint_t **h, dlistint_t **new_node, unsigned int idx)
+{
+	dlistint_t *head_crawler, *prev_node;
+	unsigned int node = 0;
+
+        head_crawler = *h;
+
+	while (head_crawler != NULL)
+	{
+		prev_node = head_crawler;
+		head_crawler = head_crawler->next;
+		++node;
+		if (node == idx)
+		{
+			(*new_node)->prev = prev_node;
+			(*new_node)->next = head_crawler;
+			prev_node->next = *new_node;
+			head_crawler->prev = *new_node;
+			return (*new_node);
+		}
+	}
+	return (NULL);
+}
+
+
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position to a dlist
  * @h: the beginning of the dlist
  * @idx: position to insert new node at
@@ -35,8 +69,8 @@ dlistint_t *insert_dnodeint_at_0(dlistint_t **h, dlistint_t **new_node)
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_dlistint_t, *head_crawler, *prev_node;
-	unsigned int no_of_nodes, node = 0;
+	dlistint_t *new_dlistint_t, *head_crawler; /* *prev_node;*/
+	unsigned int no_of_nodes; /* node = 0;*/
 
 	new_dlistint_t = (dlistint_t *)malloc(sizeof(dlistint_t));
 
@@ -52,17 +86,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_dlistint_t = insert_dnodeint_at_0(h, &new_dlistint_t);
 		return (new_dlistint_t);
-		/*new_dlistint_t->prev = NULL;
-		new_dlistint_t->next = head_crawler;
-		if (head_crawler != NULL)
-			head_crawler->prev = new_dlistint_t;
-		*h = new_dlistint_t;
-		return (new_dlistint_t);*/
 	}
 
 	else if (idx > 0 && idx < no_of_nodes)
 	{
-		while (head_crawler != NULL)
+		new_dlistint_t = insert_dnodeint_at_mid(h, &new_dlistint_t, idx);
+		return (new_dlistint_t);
+		/*while (head_crawler != NULL)
 		{
 			prev_node = head_crawler;
 			head_crawler = head_crawler->next;
@@ -75,7 +105,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				head_crawler->prev = new_dlistint_t;
 				return (new_dlistint_t);
 			}
-		}
+		}*/
 	}
 	else if (idx == no_of_nodes)
 	{
