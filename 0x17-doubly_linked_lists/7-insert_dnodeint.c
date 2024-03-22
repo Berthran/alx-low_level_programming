9b0c481c3d7b044ee8dbb8a9660165a3db1644de
Daniel Berthran <berthlearn@gmail.com>
2024-03-22 14:01:47 +0100

    correct - check for null prev pointer for insertion at index 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_dlistint_t->prev = NULL;
 		new_dlistint_t->next = head_crawler;
-		if (head_crawler->prev != NULL)
+		if (head_crawler != NULL)
 			head_crawler->prev = new_dlistint_t;
 		*h = new_dlistint_t;
 		return (new_dlistint_t);
