433f6948f2dfd3bc701f7b9052879bfcf7a4ee91
Daniel Berthran <berthlearn@gmail.com>
2024-03-22 05:46:42 +0100

    correct code for palindrome

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 def max_palindrome(pals=[]):
-    for i in range(100, 1000):
-        for j in range(100, 1000):
+    for i in range(500, 1000):
+        for j in range(500, 1000):
             prod = i * j
             prods = str(i) + " x " + str(j)
             prod_str = str(prod)
             if prod_str == prod_str[::-1]:
-                pals.append(prods)
+                pals.append(prod)
+    pals.sort()
     return (pals[-1])
 
 pals = []
