92874bf7e8d27e2fae9f3f1caf433e7f6b152f8b
Daniel Berthran <berthlearn@gmail.com>
2024-03-26 02:48:50 +0100

    double command

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P . https://github.com/Berthran/alx-low_level_programming/blob/master/0x18-dynamic_libraries/librand.so
+export LD_PRELOAD=./librand.so
 export LD_PRELOAD=./librand.so
