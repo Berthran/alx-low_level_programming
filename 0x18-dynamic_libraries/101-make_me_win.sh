bc96defecf91f6ced3d45801440b05cba1f00195
Daniel Berthran <berthlearn@gmail.com>
2024-03-26 02:48:00 +0100

    download lib into cwd

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P $HOME https://github.com/Berthran/alx-low_level_programming/blob/master/0x18-dynamic_libraries/librand.so
-export LD_PRELOAD=$HOME/librand.so
+wget -P . https://github.com/Berthran/alx-low_level_programming/blob/master/0x18-dynamic_libraries/librand.so
+export LD_PRELOAD=./librand.so
