d755ea838d0a8325545f3a1282930d077095305e
Daniel Berthran <berthlearn@gmail.com>
2024-03-26 02:17:57 +0100

    former file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-LIBRARY_URL="https://github.com/Berthran/alx-low_level_programming/blob/master/0x18-dynamic_libraries/librand.so"
-export LD_PRELOAD="$LIBRARY_URL"
+echo "export LD_PRELOAD=./librand.so" >> $HOME/.bashrc
+source $HOME/.bashrc
